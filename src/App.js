5e1b48671e6c6af43b6c9066344bc77160b5b9e8
matheswaarans <mat@nfnlabs.in>
2019-10-16 16:54:08 +0530

    Template Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>DoTik</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
